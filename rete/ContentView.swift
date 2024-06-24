ff094bc15ce21fa75d3a0a26d75af9fee1c3adac
Dip Dutt <emduttt456@gmail.com>
2024-06-25 02:05:57 +0600

    [Feature]Add rectangle in ContentView

--- rete/ContentView.swift
+++ rete/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.red)
             Text("Hello, world!")
+            Rectangle()
         }
         .padding()
     }
