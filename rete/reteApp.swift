70bbf0d2dc92d345300e573891c03f39ebc549c0
Dip Dutt <emduttt456@gmail.com>
2024-06-24 12:05:16 +0600

    Initial Commit

---
+++ rete/reteApp.swift
@@ -0,0 +1,17 @@
+//
+//  reteApp.swift
+//  rete
+//
+//  Created by Dip Dutt on 24/6/24.
+//
+
+import SwiftUI
+
+@main
+struct reteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
