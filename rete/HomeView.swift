3c0ac317ba01a3c3f9ca424f03fce1aa5b6838e7
Dip Dutt <emduttt456@gmail.com>
2024-06-24 12:10:23 +0600

    Add HomeView

---
+++ rete/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  rete
+//
+//  Created by Dip Dutt on 24/6/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .font(.title)
+            
+    }
+}
+
+#Preview {
+    HomeView()
+}
